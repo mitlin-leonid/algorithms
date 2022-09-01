32f77cf24456e17ae2ac727f7eff454dcc4e621e
mitlinleonid <mitlinleonid@gmail.com>
2022-09-01 19:29:45 +0400

    Добавлен в файл метод класса сортировки пузырьком. Метод принимает аргумент

--- bubblesortclass.py
+++ bubblesortclass.py
@@ -3,9 +3,15 @@ class Buble:
         self.name = "Я сортировщик пузырьком"
         pass
 
-    def sort(self):
-        pass
+    def sort(self, A:list):
+        N = len(A)
+        for bypass in range(1, N):
+            for k in range(0, N-bypass):
+                if A[k] > A[k+1]:
+                    A[k], A[k+1] = A[k+1], A[k]
 
 bub = Buble()
-
-print(bub.name)
+A = [3,5,2,6,7]
+print(A)
+bub.sort(A)
+print(A)
